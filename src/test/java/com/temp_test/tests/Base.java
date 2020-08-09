31a66e6df03a1d312d6cd3540f4de09fc8f58bbd
thaleshXor <thalesh.sharma@sap.com>
2020-08-09 23:07:03 +0530

    Base changed for grid

--- src/test/java/com/temp_test/tests/Base.java
+++ src/test/java/com/temp_test/tests/Base.java
@@ -1,23 +1,18 @@
 package com.temp_test.tests;
 
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
 	
